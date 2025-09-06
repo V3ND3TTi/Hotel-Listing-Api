c8bfb11842cfd2c2851caa5f00b3b2daa60d82b8
Devon Vendetti <v3nd3tti@icloud.com>
2025-09-06 13:47:52 -0700

    Performed new migration to changed Country.Id to Country.CountryId

--- HotelListingApi/Migrations/20250906204554_RenameCountryId.Designer.cs
+++ HotelListingApi/Migrations/20250906204554_RenameCountryId.Designer.cs
@@ -3,6 +3,7 @@ using HotelListingApi.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace HotelListingApi.Migrations
 {
     [DbContext(typeof(HotelListingDbContext))]
-    partial class HotelListingDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250906204554_RenameCountryId")]
+    partial class RenameCountryId
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ HotelListingApi/Migrations/20250906204554_RenameCountryId.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HotelListingApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameCountryId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Countries",
+                newName: "CountryId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CountryId",
+                table: "Countries",
+                newName: "Id");
+        }
+    }
+}
